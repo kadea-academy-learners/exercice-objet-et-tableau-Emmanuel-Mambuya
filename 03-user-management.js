f1b84141bb3a000509339ba57f6754cbc0520ad7
Emmanuel-Mambuya <emmanuelmambuya@gmail.com>
2025-10-31 12:19:08 +0100

    resolve exercise 03 about user management

--- 03-user-management.js
+++ 03-user-management.js
@@ -5,12 +5,28 @@
 // - estAdmin (boolean)
 // La fonction doit retourner un tableau contenant les noms des utilisateurs qui sont admin.
 
-function whoIsAdmin() {
-	
-}
-
-module.exports = {
+const users = [
+	{ nom: "Alice", age: 55, estAdmin: true },
+	{ nom: "Charlie", age: 27, estAdmin: false },
+  ];
+  
+  function whoIsAdmin(users) {
+	if (users.length === 0) { // si le tableau est vide
+	  return []; 
+	}
+  
+	const admins = []; // constante pour y mettre les noms des admins
+  
+	for (let i = 0; i < users.length; i++) {
+	  if (users[i].estAdmin === true) {
+		admins.push(users[i].nom); // ajoute le nom d'administrateur au tableau
+	  }
+	}
+  
+	return admins; // retourne le tableau des admins
+  }
+  
+  module.exports = {
 	whoIsAdmin,
-};
-
-
+  };
+  
